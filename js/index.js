3ce5a354cd53912b7fc1a8cba2248a0d9b16bf77
Henry Galindo <ushenrygalindo@gmail.com>
2023-07-07 18:22:33 +0200

    Added JS basics VTypes pending functions

--- js/index.js
+++ js/index.js
@@ -2,3 +2,9 @@ console.log('Nutris');
 // let const country ('Deustcheland');
 let country = "Deustcheland";
 console.log(country);
+
+let selected_colors = ['red', 'green', 'blue']
+console.log(selected_colors)
+
+const attitude = "Stay Hard"
+console.log(attitude)
